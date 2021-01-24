284efbd9b2fb4d72d45b0db6512ca9bdd45656a8
JPD <tokyo.tokyobanana@gmail.com>
2021-01-25 08:33:39 +0900

    ini-parser導入前戻し用 チェックポイント

--- ConfigConfirmationTool/Form1.cs
+++ ConfigConfirmationTool/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,41 @@ namespace ConfigConfirmationTool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 画面初期化時発生イベント
+        /// </summary>
+        /// <remarks>①チェック対象ファイル読み込み</remarks>
+        /// <param name="sender">イベント発生オブジェクト</param>
+        /// <param name="e">イベント詳細</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ①チェック対象ファイル読み込み
+            GetConfirmationList();
+        }
+
+        /// <summary>
+        /// App.config設定値取得
+        /// </summary>
+        /// <param name="key">設定値(key)</param>
+        /// <returns>設定値(value)</returns>
+        private string GetAppSetting(string key)
+        {
+            // App.configからkeyを持つvalueを呼び出し元へ返却
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        /// <summary>
+        /// チェック対象ファイル読み込み
+        /// </summary>
+        private void GetConfirmationList()
+        {
+            // チェック対象ファイル.iniファイルパス取得
+            string path = GetAppSetting("InitialFilePath");
+            // TODO: チェック対象ファイル.ini読み込み
+
+            // 
         }
     }
 }
---
+++ ConfigConfirmationTool/model/DataModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace ConfigConfirmationTool.model
+{
+    /// <summary>
+    /// 処理用モデルクラス
+    /// </summary>
+    /// <remarks>モデル</remarks>
+    public class DataModel
+    {
+        /// <summary>
+        /// 変更ID
+        /// </summary>
+        public string ChangeID { get; set; }
+
+        /// <summary>
+        /// ファイルメタ情報
+        /// </summary>
+        public List<FileInfo> FileInfos { get; set; }
+
+        /// <summary>
+        /// XML検証タグ情報
+        /// </summary>
+        public List<XmlInfo> XmlInfos { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class FileInfo
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class XmlInfo
+        {
+            public string Operation { get; set; }
+            public string ChangeData { get; set; }
+            public string Element1 { get; set; }
+            public string Element2 { get; set; }
+            public string Attribute2 { get; set; }
+            public string AttributeValue2 { get; set; }
+            public string Element3 { get; set; }
+            public string Attribute3 { get; set; }
+            public string AttributeValue3 { get; set; }
+            public string Element4 { get; set; }
+            public string Attribute4 { get; set; }
+            public string AttributeValue4 { get; set; }
+            public string Element5 { get; set; }
+            public string Attribute5 { get; set; }
+            public string AttributeValue5 { get; set; }
+            public string Element6 { get; set; }
+            public string Attribute6 { get; set; }
+            public string AttributeValue6 { get; set; }
+            public string ChangeAttribute { get; set; }
+        }
+    }
+}
